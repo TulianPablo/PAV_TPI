f0209f756c74fb92700bc36e564167d2c0a5c68e
Agustina <aguusnm@gmail.com>
2016-10-09 20:24:07 -0300

    primer commit

---
+++ PAV_TPI/PAV_TPI/Clases/CentroMedico.vb
@@ -0,0 +1,10 @@
+﻿Public Class CentroMedico
+    Public Property nro_centroMedico As Integer
+    Public Property denominacion As String
+    Public Property calle As String
+    Public Property numero As Integer
+    Public Property id_barrio As Integer
+    Public Property telefono As Integer
+    Public Property mail As String
+    Public Property fecha_baja As Date
+End Class
