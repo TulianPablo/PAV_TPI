07d52106856fd3d061d6b13cbd691aac28a8700f
Deby <deboracordobaq@gmail.com>
2016-10-11 13:30:11 -0300

    registrar nuevo afiliado

--- PAV_TPI/PAV_TPI/Principal/Afiliados/frm_nuevoAfiliado.vb
+++ PAV_TPI/PAV_TPI/Principal/Afiliados/frm_nuevoAfiliado.vb
@@ -7,7 +7,7 @@
         CargarCombo(cbo_barrio, BDHelper.GetBarrios(), "id_barrio", "nombre")
         mtb_fechaAlta.Text = Date.Today.ToString
         mtb_fechaAlta.Enabled = False
-        lbl_afiliados_mensaje.Enabled = False
+        lbl_afiliados_mensaje.Visible = False
 
     End Sub
 
@@ -20,84 +20,154 @@
 
     End Sub
 
-    Private Sub btn_aceptar_Click(sender As Object, e As FormClosingEventArgs) Handles btn_aceptar.Click
+    Private Sub btn_aceptar_Click(sender As Object, e As EventArgs) Handles btn_aceptar.Click
+        lbl_afiliados_mensaje.Visible = False
+
         If MessageBox.Show("¿Desea registrar nuevo afiliado?", "Importante", MessageBoxButtons.OKCancel, MessageBoxIcon.Question) = Windows.Forms.DialogResult.OK Then
             If validar_campos_completos() Then
-                Dim str As String = "INSERT Afiliado"
-                str &= "values('" & txt_nombre.Text & "',"
+                Dim str As String = "INSERT [dbo].[Afiliado] "
+                str &= "([nombre],[apellido],[id_tipoDoc],[nro_doc],[fecha_nac],[calle],[nro],[id_barrio],[telefono],[id_tipoAfiliado],[fecha_alta],[fecha_baja]) "
+                str &= "VALUES('" & txt_nombre.Text & "',"
                 str &= "'" & txt_apellido.Text & "',"
-                str &= cbo_tipoDoc.SelectedValue + 1 & ","
+                str &= cbo_tipoDoc.SelectedValue & ","
                 str &= txt_nroDoc.Text & ","
                 str &= "'" & dtp_fechaNac.Text & "',"
                 str &= "'" & txt_calle.Text & "',"
                 str &= txt_nroCalle.Text & ","
-                str &= cbo_barrio.SelectedValue + 1 & ","
+                str &= cbo_barrio.SelectedValue & ","
                 str &= txt_telefono.Text & ","
-                str &= cbo_tipoAfiliado.SelectedValue + 1 & ","
+                str &= cbo_tipoAfiliado.SelectedValue & ","
                 str &= "'" & mtb_fechaAlta.Text & "',"
                 str &= "NULL)"
                 BDHelper.getDBHelper.EjecutarSQL(str)
-                lbl_afiliados_mensaje.Enabled = False
+                Me.Close()
+            Else
+                lbl_afiliados_mensaje.Text = "Debe ingresar TODOS los datos"
+                lbl_afiliados_mensaje.Visible = True
             End If
-        Else
-            lbl_afiliados_mensaje.Text = "Debe ingresar TODOS los datos"
-            lbl_afiliados_mensaje.Enabled = True
         End If
+
     End Sub
 
     Private Function validar_campos_completos() As Boolean
         If String.IsNullOrEmpty(txt_nombre.Text) Then
-            MsgBox("Campo <Nombre> es obligatorio.", vbOKOnly + vbCritical, "Importante")
-            txt_nombre.Focus()
             Return False
-        Else
-            If (txt_nombre.Text >= 0) And (txt_nombre.Text <= 57) Then
-                MsgBox("El campo <Nombre> es alfabetico", vbOKOnly + vbCritical, "Importante")
-                txt_nombre.Text = 8
-                Return False
-            End If
         End If
 
         If String.IsNullOrEmpty(txt_apellido.Text) Then
-            MsgBox("Campo <Apellido> es obligatorio.", vbOKOnly + vbCritical, "Importante")
-            txt_nombre.Focus()
             Return False
-        Else
-            If (txt_nombre.Text >= 0) And (txt_nombre.Text <= 57) Then
-                MsgBox("El campo <Apellido> es alfabetico", vbOKOnly + vbCritical, "Importante")
-                txt_nombre.Text = 8
-                Return False
-            End If
         End If
 
         If String.IsNullOrEmpty(txt_nroDoc.Text) Then
-            MsgBox("Campo <Nro Documento> es obligatorio.", vbOKOnly + vbCritical, "Importante")
-            txt_nombre.Focus()
             Return False
-        Else
-            If (txt_nombre.Text >= 48) And (txt_nombre.Text <= 57) Then
-                MsgBox("El campo <Nro Documento> es numerico", vbOKOnly + vbCritical, "Importante")
-                txt_nombre.Text = 8
-                Return False
-            End If
         End If
 
-        If cbo_tipoDoc.SelectedValue = -1 And Not String.IsNullOrEmpty(txt_nroDoc.Text) Then
-            MsgBox("Falta ingresar <Tipo de Documento>", vbOKOnly + vbCritical, "Importante")
-            cbo_tipoDoc.Focus()
+        If cbo_tipoDoc.SelectedValue = -1 Then
             Return False
         End If
+
+        If String.IsNullOrEmpty(txt_calle.Text) Then
+            Return False
+        End If
+
+        If String.IsNullOrEmpty(txt_nroCalle.Text) Then
+            Return False
+        End If
+
+        If cbo_barrio.SelectedValue = -1 Then
+            Return False
+        End If
+
+        If cbo_tipoAfiliado.SelectedValue = -1 Then
+            Return False
+        End If
+
         Return True
 
     End Function
 
     Private Sub btn_cancelar_Click(sender As Object, e As EventArgs) Handles btn_cancelar.Click
-        If MessageBox.Show("¿Desea cancelar el registro? Va a perdre todos los datos cargados", "Importante", MessageBoxButtons.YesNo, MessageBoxIcon.Information, MessageBoxDefaultButton.Button2) = Windows.Forms.DialogResult.Yes Then
+        If MessageBox.Show("¿Desea cancelar el registro? Va a perder todos los datos cargados", "Importante", MessageBoxButtons.YesNo, MessageBoxIcon.Information, MessageBoxDefaultButton.Button2) = Windows.Forms.DialogResult.Yes Then
             Me.Close()
         End If
     End Sub
 
-    Private Sub MaskedTextBox2_MaskInputRejected(sender As Object, e As MaskInputRejectedEventArgs) Handles txt_apellido.MaskInputRejected
+    'Valida que el campo nombre sea alfabetico
+    Private Sub txt_nombre_KeyPress(ByVal sender As Object, ByVal e As System.Windows.Forms.KeyPressEventArgs) Handles txt_nombre.KeyPress
+        If Char.IsLetter(e.KeyChar) Then
+            e.Handled = False
+        ElseIf Char.IsControl(e.KeyChar) Then
+            e.Handled = False
+        ElseIf Char.IsSeparator(e.KeyChar) Then
+            e.Handled = False
+        Else
+            e.Handled = True
+        End If
+
+        txt_nombre.Text = Trim(Replace(txt_nombre.Text, " ", ""))
+        txt_nombre.Select(txt_nombre.Text.Length, 0)
 
     End Sub
+
+    'valida que el textBox apellido sea alfabetico
+    Private Sub txt_apellido_KeyPress(ByVal sender As Object, ByVal e As System.Windows.Forms.KeyPressEventArgs) Handles txt_apellido.KeyPress
+        If Char.IsLetter(e.KeyChar) Then
+            e.Handled = False
+        ElseIf Char.IsControl(e.KeyChar) Then
+            e.Handled = False
+        ElseIf Char.IsSeparator(e.KeyChar) Then
+            e.Handled = False
+        Else
+            e.Handled = True
+        End If
+
+        txt_apellido.Text = Trim(Replace(txt_apellido.Text, " ", ""))
+        txt_apellido.Select(txt_apellido.Text.Length, 0)
+
+    End Sub
+
+    'valida que el textbox nro documento sea numerico
+    Private Sub txt_nroDoc_KeyPress(ByVal sender As Object, ByVal e As System.Windows.Forms.KeyPressEventArgs) Handles txt_nroDoc.KeyPress
+
+        If Char.IsDigit(e.KeyChar) Then
+            e.Handled = False
+        ElseIf Char.IsControl(e.KeyChar) Then
+            e.Handled = False
+        ElseIf Char.IsSymbol(e.KeyChar) Then
+            e.Handled = False
+        ElseIf Char.IsSeparator(e.KeyChar) Then
+            e.Handled = False
+        ElseIf Char.IsWhiteSpace(e.KeyChar) Then
+            e.Handled = False
+        Else
+            e.Handled = True
+        End If
+
+        txt_nroDoc.Text = Trim(Replace(txt_nroDoc.Text, "  ", " "))
+        txt_nroDoc.Select(txt_nroDoc.Text.Length, 0)
+
+    End Sub
+
+    'valida que el textbox nro documento sea numerico
+    Private Sub txt_nroCalle_KeyPress(ByVal sender As Object, ByVal e As System.Windows.Forms.KeyPressEventArgs) Handles txt_nroCalle.KeyPress
+
+        If Char.IsDigit(e.KeyChar) Then
+            e.Handled = False
+        ElseIf Char.IsControl(e.KeyChar) Then
+            e.Handled = False
+        ElseIf Char.IsSymbol(e.KeyChar) Then
+            e.Handled = False
+        ElseIf Char.IsSeparator(e.KeyChar) Then
+            e.Handled = False
+        ElseIf Char.IsWhiteSpace(e.KeyChar) Then
+            e.Handled = False
+        Else
+            e.Handled = True
+        End If
+
+        txt_nroCalle.Text = Trim(Replace(txt_nroCalle.Text, "  ", " "))
+        txt_nroCalle.Select(txt_nroDoc.Text.Length, 0)
+
+    End Sub
+
 End Class
