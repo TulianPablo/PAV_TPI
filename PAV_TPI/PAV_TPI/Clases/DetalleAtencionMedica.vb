75d6d1448e67c351f96b9f62311d0a4ffa04572f
Deby <deboracordobaq@gmail.com>
2016-10-18 18:50:20 -0300

    Detalle Atencion Medica - Pablo Tulian

---
+++ PAV_TPI/PAV_TPI/Clases/DetalleAtencionMedica.vb
@@ -0,0 +1,10 @@
+﻿Public Class DetalleAtencionMedica
+    Property fecha_atencion As Date
+    Property ordinal As Integer
+    Property id_practica As Integer
+    Property nombre_practica As String
+    Property porc_descuento As Double
+    Property precio_practica As Double
+    Property subtotal As Double
+
+End Class
